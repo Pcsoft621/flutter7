7124241e5ecd88bc529752928f71fcc154ac96fe
priya <pcsoft621@gmail.com>
2022-08-10 16:32:47 +0530

    priya deleted

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter priya home page'),
+      home: const MyHomePage(title: 'Flutter  home page'),
     );
   }
 }
